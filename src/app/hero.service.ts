6598ee9f4c0c6df9317733d1922d6f52ed0e5357
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2023-10-04 23:47:53 -0300

    serviços http habitados

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
+  
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
     const heroes = of (HEROES);
     this.messageService.add('HeroService: fetdhed heroes');
     return heroes;
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero>{
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fatched hero id=${id}`);
+    return of(hero);
+  }
+
 }
